0cc51d59c50933992bb52bb50661f42bf811cc30
bijikatul17 <81357460+bijikatul17@users.noreply.github.com>
2022-07-05 06:12:56 +0700

    Update config.php

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ $database = "luqman_tri_bimantoro";
  
 $conn = mysqli_connect($server, $user, $pass, $database);
  
-if (!$conn) {
-    die("<script>alert('Gagal tersambung dengan database.')</script>");
-}
+#if (!$conn) {
+#    die("<script>alert('Gagal tersambung dengan database.')</script>");
+#}
  
 ?>
